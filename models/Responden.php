92c6c845f168127b9e00412439322622b2e4710f
Ego Dafma Dasa <egodasa@users.noreply.github.com>
2019-12-11 15:06:04 +0700

    Create Responden.php

---
+++ models/Responden.php
@@ -0,0 +1,20 @@
+<?php
+require_once "models.php";
+class Responden extends Models {
+  function __construct()
+  {
+    parent::__construct();
+    $this->tabel = "tbl_responden";
+    $this->primaryKey = "responden_id";
+    $this->kolomBawaanCrud = [
+      "responden_nama",
+      "responden_usia",
+      "responden_jk",
+      "responden_pendidikan",
+      "responden_masa_kerja",
+      "responden_status_sosial"
+     ]; // daftar kolom pada tabel kecuali primary keynya
+    // $this->view = "data_pertanyaan";
+   
+  }
+}
