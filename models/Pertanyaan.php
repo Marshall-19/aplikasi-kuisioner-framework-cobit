c3800c76e51ff77f136b4031c07946adfc8c6552
Ego Dafma Dasa <egodasa@users.noreply.github.com>
2019-12-11 13:42:41 +0700

    Create Pertanyaan.php

---
+++ models/Pertanyaan.php
@@ -0,0 +1,13 @@
+<?php
+require_once "models.php";
+class Pertanyaan extends Models {
+  function __construct()
+  {
+    parent::__construct();
+    $this->tabel = "tbl_pertanyaan";
+    $this->primaryKey = "pertanyaan_id";
+    $this->kolomBawaanCrud = ["pertanyaan", "kategori_id"]; // daftar kolom pada tabel kecuali primary keynya
+    $this->view = "data_pertanyaan";
+   
+  }
+}
