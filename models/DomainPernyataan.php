5e103d13331dbf553a5c40b11bc8a6d67f78574a
Ego Dafma Dasa <egodasa@gmail.com>
2019-12-16 11:45:32 +0700

    Fix bug ajax

--- models/DomainPernyataan.php
+++ models/DomainPernyataan.php
@@ -18,15 +18,15 @@ class DomainPernyataan extends Models {
     return $this->db->query("Select
                                   COUNT(distinct tbl_kuisioner.responden_id) AS responden_total,
                                   SUM(tbl_jawaban_kuisioner.skor) AS skor_total,
-                                  tbl_domain_pernyataan.domain_keterangan,
-                                  tbl_domain_pernyataan.domain_nama,
-                                  tbl_domain_pernyataan.domain_id,
+                                  tbl_domain.domain_keterangan,
+                                  tbl_domain.domain_nama,
+                                  tbl_domain.domain_id,
                                   COUNT(distinct tbl_jawaban_kuisioner.pernyataan_id) AS pernyataan_total 
                               From
                                   tbl_kuisioner Inner Join
                                   tbl_jawaban_kuisioner On tbl_jawaban_kuisioner.kuisioner_id = tbl_kuisioner.kuisioner_id Inner Join
                                   tbl_pernyataan On tbl_jawaban_kuisioner.pernyataan_id = tbl_pernyataan.pernyataan_id Inner Join
-                                  tbl_domain_pernyataan On tbl_pernyataan.domain_id = tbl_domain_pernyataan.domain_id WHERE tbl_domain_pernyataan.domain_id = :domain_id  
-                              GROUP BY tbl_domain_pernyataan.domain_id;", ["domain_id" => $domain_id])->fetch(PDO::FETCH_ASSOC);
+                                  tbl_domain On tbl_pernyataan.domain_id = tbl_domain.domain_id WHERE tbl_domain.domain_id = :domain_id  
+                              GROUP BY tbl_domain.domain_id", ["domain_id" => $domain_id])->fetch(PDO::FETCH_ASSOC);
   }
 }
